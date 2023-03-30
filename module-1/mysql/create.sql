65ac0ca0fdf659a0cba9aa007d8bceeb8a8f0600
magnofz <magnofz@gmail.com>
2023-03-30 09:48:45 +0200

    lab mysql

---
+++ module-1/mysql/create.sql
@@ -0,0 +1,41 @@
+CREATE TABLE Cars
+(
+	ID INT NOT NULL
+	, VIN VARCHAR(50) NOT NULL
+	, Manufacturer VARCHAR(50) NOT NULL
+	, Model VARCHAR(50) NOT NULL
+	, "Year" INT NOT NULL
+	, Color VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE Customers
+(
+	ID INT NOT NULL
+	, "Customer ID" INT NOT NULL
+	, Name VARCHAR(50) NOT NULL
+	, Phone VARCHAR(50) NOT NULL
+	, Email VARCHAR(50) NOT NULL
+	, Address VARCHAR(50) NOT NULL
+	, City VARCHAR(50) NOT NULL
+	, "State/Province" VARCHAR(50) NOT NULL
+	, Country VARCHAR(50) NOT NULL
+	, Postal INT NOT NULL
+);
+
+CREATE TABLE SalesPersons
+(
+	ID INT NOT NULL
+	, "Staff ID" INT NOT NULL
+	, Name VARCHAR(50) NOT NULL
+	, Store VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE Invoices
+(
+	ID INT NOT NULL
+	, "Invoice Number" VARCHAR(50) NOT NULL
+	, "Date" DATE NOT NULL
+	, Car INT NOT NULL
+	, Customer INT NOT NULL
+	, "Sales Person" INT NOT NULL
+);
---
+++ module-1/mysql/update.sql
@@ -0,0 +1,4 @@
+UPDATE SalesPersons SET Store = 'Miami' WHERE ID = 4;
+UPDATE Customers SET Email = 'ppicaso@gmail.com' WHERE Name = 'Pablo Picasso';
+UPDATE Customers SET Email = 'lincoln@us.gob' WHERE Name = 'Abraham Lincoln';
+UPDATE Customers SET Email = 'hello@napoleon.me' WHERE Name = 'Napoléon Bonaparte';
